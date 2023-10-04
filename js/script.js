a89e5c35c28190e992ed280602b6de3a62479fae
Anna <annmarinkova@gmail.com>
2023-10-04 21:48:06 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const answers = [];
+answers[0] = prompt('Please enter your name', '');
+answers[1] = prompt('Please enter your age', '');
+answers[2] = prompt('Please enter your city', '');
+document.write(answers);
+
+const category = 'books';
+console.log(`https://url.com/${category}/5`);
+
+const user = 'Ann';
+alert(`Hello, ${user}!`);
+
+console.log(18 + + '2');
+console.log(100 + + '4');
+
+let incr = 10,
+	decr = 10;
+// incr++;
+// decr--;
+console.log(incr++);
+console.log(--decr);
